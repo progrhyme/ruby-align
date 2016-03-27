83e69ce91b7f4ebffb9265d4cb7964fd9074c40f
key-amb <yasutake.kiyoshi@gmail.com>
2016-03-28 00:33:05 +0900

    Add a complex test

---
+++ spec/expects/301_nested_logic_blocks.rb
@@ -0,0 +1,32 @@
+%w[ok ng anonymous].each do |ans|
+  foo        = 1
+  x86      ||= true
+  @a_value   = []
+  if ans.size > 3
+    p 'I do not like it.'
+    stash = {
+      record:       true,
+      'contents' => 'Blah blah blah.'
+    }
+    loop do
+      unwanted  = nil
+      @@verbose = "True"
+      break
+    end
+  end
+end
+
+def self.hoo x, y, z
+  @x    = x
+  $yyy  = y || z
+  a    << z
+  if a.length > 10
+    tree = {
+      name:   'long',
+      'id' => nil,
+    }
+  else
+    one = 'two'
+    @x  = x + y + z
+  end
+end
